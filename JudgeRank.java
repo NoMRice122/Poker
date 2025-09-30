2c4208feccd95e383922cd0998095ce5d785a0cc
NoMRice122 <seia.ice515122@gmail.com>
2025-09-30 12:20:03 +0900

    add

--- JudgeRank.java
+++ JudgeRank.java
@@ -1,8 +1,10 @@
 public class JudgeRank {
     private final Hand hand;
+    private final Card[] cards;
 
     public JudgeRank(Hand hand) {
         this.hand = hand;
+        this.cards = hand.getCards();
     }
 
     /*
@@ -14,7 +16,7 @@ public class JudgeRank {
     public String doJudge() {
         // TODO: 役判定ロジックを実装
         if(isRF(hand)){return "RF";}
-        if(isST(hand) && isFL(hand)){return "SF";}
+        if(isSF(hand)){return "SF";}
         if(is4K(hand)){return "4K";}
         if(isFH(hand)){return "FH";}
         if(isFL(hand)){return "FL";}
@@ -32,40 +34,62 @@ public class JudgeRank {
     //ロイヤルストレートフラッシュ
     public boolean isRF(Hand hand){
 
+        return false;
     }
 
-    //フラッシュ
-    public boolean isFL(Hand hand){
+    //ストレートフラッシュ(完成)
+    public boolean isSF(Hand hand){
+        if(isST(hand) && isFL(hand)){
+            return true;
+        }
+        return false;
+    }
 
+    //フラッシュ(完成)
+    public boolean isFL(Hand hand){
+        //1つ目のカードのスートを基準にして、他のカードも同じか検証する
+        Suit criterion = cards[0].getSuit();
+        for(int i=1; i<cards.length; i++){
+            if(cards[i].getSuit() != criterion){
+                return false;
+            }
+        }
+        return true;
     }
 
     //ストレート
     public boolean isST(Hand hand){
-
+        
+        return false;
     }
 
     //フォーカード
     public boolean is4K(Hand hand){
 
+        return false;
     }
 
     //フルハウス
     public boolean isFH(Hand hand){
 
+        return false;
     }
 
     //スリーカード
     public boolean is3K(Hand hand){
 
+        return false;
     }
 
     //ツーペア
     public boolean is2P(Hand hand){
 
+        return false;
     }
 
     //ワンペア
     public boolean is1P(Hand hand){
 
+        return false;
     }
 }
